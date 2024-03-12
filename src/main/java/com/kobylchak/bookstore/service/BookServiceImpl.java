d9399df5d04b51b1f5c6184ba287b231c211449e
Nonzeq <bgdn.kobylchak@gmail.com>
2024-03-12 16:16:22 +0200

    configure spring boot, added Book entity with service  and repository implementation

---
+++ src/main/java/com/kobylchak/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package com.kobylchak.bookstore.service;
+
+import com.kobylchak.bookstore.model.Book;
+import com.kobylchak.bookstore.repository.BookRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
