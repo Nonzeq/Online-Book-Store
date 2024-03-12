aa9867da5832c642fecccee0b145f75418e083fe
Nonzeq <bgdn.kobylchak@gmail.com>
2024-03-12 12:02:37 +0200

    initial commit

---
+++ src/test/java/com/kobylchak/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kobylchak.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
