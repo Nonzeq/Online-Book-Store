b9c77a9df25b74e57619c8ffd975af43dd9460d9
Nonzeq <bgdn.kobylchak@gmail.com>
2024-03-23 16:35:14 +0200

    added swagger and pagination

--- src/main/java/com/kobylchak/bookstore/service/BookService.java
+++ src/main/java/com/kobylchak/bookstore/service/BookService.java
@@ -4,11 +4,12 @@ import com.kobylchak.bookstore.dto.book.BookDto;
 import com.kobylchak.bookstore.dto.book.BookSearchParameters;
 import com.kobylchak.bookstore.dto.book.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto book);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto getBookById(Long id);
 
