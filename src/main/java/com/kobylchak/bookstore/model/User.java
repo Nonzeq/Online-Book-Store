d31f75bebad129494f73f50202730b57b68ab223
Nonzeq <bgdn.kobylchak@gmail.com>
2024-03-27 16:30:10 +0200

    added user model to project and liquibase

---
+++ src/main/java/com/kobylchak/bookstore/model/User.java
@@ -0,0 +1,26 @@
+package com.kobylchak.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "users")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, name = "first_name")
+    private String firstname;
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+}
