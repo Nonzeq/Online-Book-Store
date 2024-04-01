06e78d608cfc61c011bd4a0936e18d7a1ecfee9e
Nonzeq <bgdn.kobylchak@gmail.com>
2024-04-01 14:43:59 +0300

    remove redurant methods

--- src/main/java/com/kobylchak/bookstore/repository/user/UserRepository.java
+++ src/main/java/com/kobylchak/bookstore/repository/user/UserRepository.java
@@ -9,7 +9,4 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("from User u join fetch u.roles where u.email = :email")
     Optional<User> findUserByEmail(String email);
-
-    @Query("from User u join fetch u.roles")
-    List<User> findAllUsers();
 }
